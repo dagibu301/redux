565cd5ee387352c1954e48cfa39ce0f637f4a029
David Giraldo <davidgiraldobueno@gmail.com>
2021-12-21 10:52:36 -0500

    Delete lines

--- src/store/index.js
+++ src/store/index.js
@@ -1,10 +1,6 @@
 import { configureStore } from '@reduxjs/toolkit';
-
 import counterReducer from './counterSlice';
 import authReducer from './authSlice';
-
-
-
 
 const store = configureStore({
   reducer: {
